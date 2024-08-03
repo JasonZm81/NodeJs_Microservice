ca9aff37c7c723a823505cc6a50e7707f2feb3ac
Jason Ooi <cchooi@CCs-MacBook-Air.local>
2024-08-03 17:33:00 +0800

    clearing

--- product_service/src/dto/service-input.ts
+++
@@ -1,7 +0,0 @@
-import { Length } from "class-validator";
-
-export class ServiceInput {
-  action: string; // optional // GET_PRODUCT // GET_CATEGORY etc
-  @Length(12, 24)
-  productId: string;
-}
